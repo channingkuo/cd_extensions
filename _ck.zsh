ab0b34975f88d0e9c0f0585e1ff86ff95991c1c0
ChanningKuo <channingkuo@icloud.com>
2025-04-03 17:57:23 +0800

    允许配置文件放到.git文件夹中

--- _ck.zsh
+++ _ck.zsh
@@ -88,6 +88,62 @@ ck() {
             fi
         done < "$CONFIG_FILE"
     fi
+  else
+    if [ -f ".git/$CONFIG_FILE" ]; then
+        echo ""
+        echo -e "${red}Project environments${nc}"
+        while IFS= read -r line; do
+            echo -e "${green}${line}${nc}"
+        done < ".git/$CONFIG_FILE"
+
+        CONDA_ENV=$(grep "Conda Env:" ".git/$CONFIG_FILE" | awk -F ': ' '{print $2}')
+        if [ -n "$CONDA_ENV" ]; then
+            echo ""
+            echo -e "${red}Swicthing Conda Env: $CONDA_ENV${nc}"
+            conda activate "$CONDA_ENV"
+        fi
+        # 提取Java环境参数
+        JAVA_ENV=$(grep "Java:" ".git/$CONFIG_FILE" | awk -F ': ' '{print $2}')
+        if [ -n "$JAVA_ENV" ]; then
+            echo ""
+            echo -e "${red}Switching to Java: $JAVA_ENV${nc}"
+            case "$JAVA_ENV" in
+                8)
+                    # jdk8
+                    export JAVA_HOME=$(/usr/libexec/java_home -v1.8)
+                    echo -e "${green}Done${nc}"
+                    ;;
+                11)
+                    # jdk11
+                    export JAVA_HOME=$(/usr/libexec/java_home -v11)
+                    echo -e "${green}Done${nc}"
+                    ;;
+                17)
+                    # jdk17
+                    export JAVA_HOME=$(/usr/libexec/java_home -v17)
+                    echo -e "${green}Done${nc}"
+                    ;;
+                *)
+                    echo -e "${red}Unknown Java version: $JAVA_ENV${nc}"
+                    ;;
+            esac
+        fi
+
+        # 检查是否有特殊的逻辑，比如临时添加export
+        # 读取并执行export命令
+        Export_ENV=$(grep "Export:" ".git/$CONFIG_FILE" | awk -F ': ' '{print $2}')
+        if [ -n "$Export_ENV" ]; then
+            echo ""
+            echo -e "${red}Extra Export${nc}"
+            while IFS= read -r line; do
+                if [[ "$line" == Export:* ]]; then
+                    export_command=$(echo "$line" | sed 's/^Export:/export/')
+                    echo -e "${green}Executing: $export_command${nc}"
+                    eval "$export_command"
+                fi
+            done < ".git/$CONFIG_FILE"
+        fi
+    fi
   fi
 
   # 打印前端项目的依赖和脚本
